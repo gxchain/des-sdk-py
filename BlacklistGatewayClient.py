f8df6fdd42f8b725047a90b9b3c09bc8b18917bc
GXCHAIN <287590296@qq.com>
2018-10-08 15:18:26 +0800

    v1.1 fix bug for signature

--- BlacklistGatewayClient.py
+++ BlacklistGatewayClient.py
@@ -3,8 +3,10 @@ import requests
 import time
 import Common.util as util
 import json
+from eth_utils import encode_hex
 import logging
 log = logging.getLogger(__name__)
+
 
 class BlacklistGatewayClient(Client.DESClient):
     """get question token
@@ -13,11 +15,12 @@ class BlacklistGatewayClient(Client.DESClient):
        @param loanInfoList
        @returns {Promise<any>}
     """
+
     def getQuestionToken(self, requestId, redirectUrl, loanInfoList):
         getTokenParams = { "requestId": requestId,
                            "redirectUrl": redirectUrl,
                            "loadInfos": loanInfoList}
-        response = requests.post("https://survey.gxb.io/blacklist/token", data=getTokenParams)
+        response = requests.post(self.baseUrl + '/token', data=getTokenParams)
         if response.status_code == requests.codes.ok:
             return response.json()
         else:
@@ -28,12 +31,16 @@ class BlacklistGatewayClient(Client.DESClient):
        @param token
        @returns {Promise<any>}
     """
+
     def getQuestionReport(self, token):
         getQuestionReportParmas = {'token': token,
                                    'timestamp': int(time.time()) + 60
                                    }
-        getQuestionReportParmas['signature'] = util.sign(bytes(str(getQuestionReportParmas), 'utf8'), self.privateKey)
-        response = requests.post('https://survey.gxb.io/blacklist/question/report', data=bytes(str(getQuestionReportParmas), "utf8"), headers={'content-type': 'application/json'})
+        tempSig = util.sign(bytes(str(getQuestionReportParmas), 'utf8'), self.privateKey)
+        getQuestionReportParmas['signature'] = encode_hex(tempSig)[2:]
+        print(getQuestionReportParmas)
+        response = requests.post(self.baseUrl + '/question/report', data=json.dumps(getQuestionReportParmas), headers={'content-type': 'application/json'})
+        print(response.content, response.text)
         if response.status_code == requests.codes.ok:
             return response.json()
         else:
@@ -41,6 +48,6 @@ class BlacklistGatewayClient(Client.DESClient):
 
 
 if __name__ == "__main__":
-    client = BlacklistGatewayClient('5KFachrDu7yHqhDeqdshedh6cWasLDv8d8Rko2JuvKM12345678', 'hzy13500')
+    client = BlacklistGatewayClient('5K8iH1jMJxn8TKXXgHJHjkf8zGXsbVPvrCLvU2GekDh2nk4ZPSF', '1.2.323', 'https://survey.gxb.io/blacklist')
     print(client.getQuestionReport("GXC4ywUcU8h6zPqESvAMkGREmmg9r54etHTpEtBHp8Rg2WYAcmFnD"))
     # print(client.getQuestionToken())
